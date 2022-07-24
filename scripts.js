0748679c37225fc3c52173cb865490dfb8de912f
Steve Ngai <cheeweng.ngai@affinhwang.com>
2022-07-24 14:37:16 +0800

    .

--- scripts.js
+++ scripts.js
@@ -11,19 +11,21 @@ $(document).ready(function () {
 const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
-
+debugger
 showHideColumn(3, false);
 showHideColumn(4, false);
 showHideColumn(5, false);
 showHideColumn(6, false);
 showHideColumn(7, false);
-
+showHideColumn(8, false);
+debugger
 if(params.id == 's'){
   showHideColumn(3, true);
   showHideColumn(4, true);
   showHideColumn(5, true);
   showHideColumn(6, true);
   showHideColumn(7, true);
+  showHideColumn(8, true);
 }
 
 function showHideColumn(col_no, do_show) {
